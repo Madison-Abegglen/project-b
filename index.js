2cf461f63659ea790891afba7d809c21dc2ffa0a
Madison Abegglen <madison.abegglen@gmail.com>
2018-11-26 15:17:58 -0700

    user stuff

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 let express = require('express')
 let bp = require('body-parser')
 let server = express()
-let port = 3000
+const PORT = process.env.PORT || 3000
 
 require('./server/db/mlab-config')
 
 // middleware
-server.use(express.static(__dirname + '/public')) // connection to front-end
 server.use(bp.json())
 server.use(bp.urlencoded({ extended: true }))
+server.use(express.static(__dirname + '/public')) // connection to front-end
 
 // routes
-let userRoutes = require('./server/routes/user-routes')
+let userRoutes = require('./server/auth/auth-routes')
 let postRoutes = require('./server/routes/post-routes')
 let commentRoutes = require('./server/routes/comment-routes')
 
-server.use('/api/users', userRoutes)
+server.use('/account', userRoutes)
 server.use('/api/posts', postRoutes)
 server.use('/api/comments', commentRoutes)
 
-// the catch-all
-server.get('*', (req, res, next) => {
-  res.status(404).send("NO MATCHING ROUTE")
+server.use('*', (req, res, next) => {
+  if (req.method == "GET") {
+    return next()
+  }
+  next(new Error("Please login to continue"))
+  // makes sure user is logged in & has permission to create/delete posts, make comments, etc
 })
+
+// the catch-all
+server.use('*', (error, req, res, next) => {
+  res.status(error.status || 404).send({ message: error.message })
+})
+
+// sets server to listen on your specific port 
 server.listen(port, () => {
   console.log("SERVER RUNNING ON PORT: ", port)
 })
--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,23 @@
 let mongoose = require('mongoose')
+let bcrypt = require('bcryptjs')
 let Schema = mongoose.Schema
+const SALT = 15
+let name = "User"
 
-// define schema
 let schema = new Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true }
+  email: { type: String, required: true, unique: true },
+  hash: { type: String, required: true },
+  favorites: { type: String }
 })
 
-module.exports = mongoose.model('User', schema)
+schema.statics.hashPassword = function (password) {
+  return bcrypt.hashSync(password, SALT)
+}
+
+schema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.hash) // use async to prevent bottlenecking
+}
+
+let model = mongoose.model(name, schema)
+
+module.exports = model
